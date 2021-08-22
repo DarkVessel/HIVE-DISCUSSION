ffef9996575f7ae9e195e54ff84107ef915d2ba0
ClintFlames <clintf3000@gmail.com>
2021-08-22 18:16:30 +0300

    Extend code.

--- server/api.js
+++ server/api.js
@@ -1,21 +1,39 @@
 /* IMPORTS AND VARIABLES */
+const { table } = require("mouse-db.json");
+const users = {
+	unsafe: new table("users.unsafe"),
+	safe: new table("users.safe")
+}
+const projects = new table("projects");
+
 const express = require("express");
 const api = express.Router();
+
+const global_path = __dirname.split("/").slice(0, __dirname.split("/").length - 1).join("/");
+
+
 
 // MiddleWare for parsing body to object (check req.body).
 api.use(express.json({ limit: "100kb" }));
 api.use(express.urlencoded({ limit: "100kb", extended: true }));
 
 // MiddleWare for check session if needed.
-api.post("/", (req, res, next) => {
-	if (require(__globalname + "/db/config.json").api.require_api_key) {
-		
+api.use((req, res, next) => {
+	if (req.body.api_key) {
+		if (users.unsafe.get().find((v) => { return v.api_key == req.body.api_key })) {
+			req.body.user = res.send(JSON.stringify(users.safe.get().find((v) => { return v.id == req.params.id; })));
+			return next();
+		}
 	}
-	next();
+	return res.send("[\"error\",0]");
 });
-api.post("/accounts", (req, res) => {
-		console.log(req.body)
-	}
-);
+
+
+
+api.post("/users", (req, res) => { res.send(JSON.stringify(users.safe.get())); });
+api.post("/users/get/:id", (req, res) => { res.send(JSON.stringify(users.safe.get().find((v) => { return v.id == req.params.id; }))); });
+api.post("/users/edit/:id", (req, res) => {
+	
+});
 
 module.exports = api;
